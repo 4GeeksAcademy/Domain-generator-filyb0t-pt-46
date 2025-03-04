ac2f0d838cd4adb0cc8c77c4fa33efedfa8e6eee
Felix Uriel Lara Castro <138715170+filyb0t@users.noreply.github.com>
2025-03-04 00:04:13 +0000

    Domain generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,34 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let articles = ["the", "an", "some", "another", "any"];
+  let adjectives = ["funny", "great", "stunning", "awesome", "thrilling"];
+  let nouns = ["website", "blog", "webapp", "spot", "hub"];
+  let tlds = [
+    ".com",
+    ".space",
+    ".org",
+    ".cln",
+    ".rocks",
+    ".cat",
+    ".dog",
+    ".blockbuster"
+  ];
+
+  let domainNames = [];
+
+  for (let article of articles) {
+    for (let adjective of adjectives) {
+      for (let noun of nouns) {
+        for (let tld of tlds) {
+          domainNames.push(`${article}${adjective}${noun}${tld}`);
+        }
+      }
+    }
+  }
+
+  document.body.innerHTML = `<ul>${domainNames
+    .map(domainName => `<li>${domainName}</li>`)
+    .join("")}</ul>`;
   console.log("Hello Rigo from the console!");
 };
